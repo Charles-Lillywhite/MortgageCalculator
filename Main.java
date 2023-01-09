d90e49ddf52acdd8991b32604bca28d359004d7e
Charles-Lillywhite <115148482+Charles-Lillywhite@users.noreply.github.com>
2023-01-09 20:36:15 +0000

    add helper function for calculating repayment

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 public class Main {
+    // useful constants
+    private static final int MONTHS_IN_YEAR = 12;
+    private static final int PERCENT = 100;
+    // min and max permitted values
+    private static final int MAX_LOAN = 1_000_000; //GBP
+    private static final int MIN_LOAN = 50_000;
+    private static final int MAX_TERM = 35;
+    private static final int MIN_TERM = 5;
+    private static final float MAX_RATE = 15f;
+    private static final float MIN_RATE = 0.05f;
     public static void main(String[] args) {
-        // useful constants
-        final int MONTHS_IN_YEAR = 12;
-        final int PERCENT = 100;
-        // min and max permitted values
-        final int MAX_LOAN = 1_000_000; //GBP
-        final int MIN_LOAN = 50_000;
-        final int MAX_TERM = 35;
-        final int MIN_TERM = 5;
-        final float MAX_RATE = 15f;
-        final float MIN_RATE = 0.05f;
 
         //initialise variables
         float principal = 0;
@@ -21,8 +21,8 @@ public class Main {
 
         Scanner inputs = new Scanner(System.in);
 
-        // Enter the loan amount ('Principal')
-        
+        // Enter & validate the loan amount
+
         while (principal < MIN_LOAN | principal > MAX_LOAN) {
             System.out.println("Enter Loan Amount between £50,000 - £1,000,000 : ");
             String input = inputs.nextLine();
@@ -34,8 +34,8 @@ public class Main {
             }
         }
 
-        // Enter the yearly interest rate
-        
+        // Enter & validate the interest rates
+
         while (yearlyRate < MIN_RATE | yearlyRate > MAX_RATE){
             System.out.println("Enter Annual Interest Rate between 0.05% and 15.00%: ");
             String input = inputs.nextLine();
@@ -48,8 +48,8 @@ public class Main {
             }
         }
 
-        // Enter the term of the loan (years)
-        
+        // Enter & validate the term of the loan
+
         while (termYears < MIN_TERM | termYears > MAX_TERM){
             System.out.println("Enter the term of the loan (5 - 35 Years Inclusive).: ");
             String input = inputs.nextLine();
@@ -62,11 +62,17 @@ public class Main {
             }
         }
 
-        double compoundRate = Math.pow((1+monthlyRate), termMonths);
-        double monthlyRepayment = principal * monthlyRate * compoundRate / (compoundRate - 1);
-
-        String outputFormatted = "£" + String.format("%.2f", monthlyRepayment);
-        System.out.println("Monthly repayment = " + outputFormatted);
-
+        String monthlyRepayment = calculateMonthlyPayment(monthlyRate, termMonths, principal);
+        System.out.println("Monthly repayment = " + monthlyRepayment);
     }
+    
+    // helper function, calculate monthly repayment
+    
+    private static String calculateMonthlyPayment(float mRate, float mTerm, float amount ){
+        double compoundRate = Math.pow((1+mRate), mTerm);
+        double monthlyRepayment = amount * mRate * compoundRate / (compoundRate - 1);
+        String outputFormatted = "£" + String.format("%.2f", monthlyRepayment);
+        return outputFormatted;
+        }
+
 }
